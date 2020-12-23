4e3eee7a34988dad14b7a4b8eb7ae38d245b7076
Jonathan Li <30177086+MonliH@users.noreply.github.com>
2020-12-23 12:20:41 -0500

    Make some styles work on ssr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Layout from "@components/Layout";
 import HomePage from "@components/home/Home";
 
-export default function Home() {
+const Home = () => {
   return (
     <Layout title="Jonathan Li" description="Jonathan's personal website.">
       <HomePage />
     </Layout>
   );
-}
+};
+
+export default Home;
