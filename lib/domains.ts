0683a5617fd1488a53cad229821052773ce9c191
Jonathan Li <30177086+MonliH@users.noreply.github.com>
2020-12-26 15:33:29 -0500

    Switch back to monolith.vision

--- lib/domains.ts
+++ lib/domains.ts
@@ -1,4 +1,4 @@
 // For development
-export const API_DOMAIN = "http://localhost:8080";
+// export const API_DOMAIN = "http://localhost:8080";
 
-// export const API_DOMAIN = "https://api.monolith.vision";
+export const API_DOMAIN = "https://api.monolith.vision";
