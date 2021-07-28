83a1cbc158a69654210e4450385df3960c07c528
Monlih <MonliH@users.noreply.github.com>
2021-07-28 11:34:31 -0400

    Make embed image show

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -22,6 +22,7 @@ const Layout = ({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content={theme} />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+        <meta property="og:image" content="android-chrome-512x512.png" />
         <link
           href="/favicon-16x16.png"
           rel="icon"
