f9686d436e98a8b4a0763bf2980debff9c9db42f
Jonathan Li <30177086+MonliH@users.noreply.github.com>
2024-06-16 00:43:24 -0400

    Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico"></link>
+      </head>
       <body>{children}</body>
     </html>
   );
